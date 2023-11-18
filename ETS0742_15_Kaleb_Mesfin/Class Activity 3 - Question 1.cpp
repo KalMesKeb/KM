08b32e987031929a96f304b60dc5c02dfee87e05
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2023-11-17 23:54:36 -0800

    Update Class Activity 3 - Question 1.cpp

--- ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 1.cpp
+++ ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 1.cpp
@@ -1,1 +1,20 @@
+// This Program calculates your BMI
+#include <iostream>
 
+using namespace std;
+
+int main(){
+	
+	float Height, Weight;
+	
+	cout << "Enter your height in meters: ";
+	cin >> Height;
+	
+	cout << "Enter your weight in kilograms: ";
+	cin >> Weight;
+	
+	cout << "Your BMI is: " << Weight / (Height * Height) << endl;
+	
+	return 0;
+	
+}
