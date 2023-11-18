086edec452550c540bf04de333b35c58a3de13f6
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2023-11-17 23:57:34 -0800

    Update Class Activity 3 - Question 3.cpp

--- ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 3.cpp
+++ ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 3.cpp
@@ -1,1 +1,30 @@
+// This program changes capital letters to small ones and vise versa
+#include <iostream>
+#include <ctype.h>
 
+using namespace std;
+
+int main(){
+	
+	char a;
+	
+	cout << "Enter a letter: ";
+	cin >> a;
+	
+	if (isupper(a)){
+		a = tolower(a);
+		cout << a << endl;
+	}
+	
+	else if (islower(a)){
+		a = toupper (a);
+		cout << a << endl;
+	}
+	
+	else {
+		cout << "That is not a letter." << endl;
+	}
+		
+	return 0;
+
+}
