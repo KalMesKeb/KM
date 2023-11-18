8dce499cbae6ee046eebc02afb041786b0c73c16
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2023-11-17 23:58:32 -0800

    Update Class Activity 3 - Question 4.cpp

--- ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 4.cpp
+++ ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 4.cpp
@@ -1,1 +1,21 @@
+// This program calculates the power of numbers you enter
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+int main(){
+	
+	double x, y;
+	
+	cout << "Enter the base: ";
+	cin >> x;
+	
+	cout << "Enter the exponent: ";
+	cin >> y;
+	
+	cout << "The result of the expression: " << pow (x, y) << endl;
+	
+	return 0;
+
+}
