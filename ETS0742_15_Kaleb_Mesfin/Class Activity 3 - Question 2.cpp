d9485efaebbd2b7d4004f2729bb0c161f4458dc4
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2023-11-17 23:56:15 -0800

    Update Class Activity 3 - Question 2.cpp

--- ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 2.cpp
+++ ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 2.cpp
@@ -1,1 +1,20 @@
+// This program calculates the maximum number of miles your car can go without refueling 
+#include <iostream>
 
+using namespace std;
+
+int main(){
+	
+	float Capacity, MPG;
+	
+	cout << "Enter your car\'s fuel tank capacity in gallons: ";
+	cin >> Capacity;
+	
+	cout << "Enter your car\'s travel distance per each gallon in miles: ";
+	cin >> MPG;
+	
+	cout << "You can drive your car " << Capacity * MPG << " miles without refueling." << endl;
+	
+	return 0;
+	
+}
