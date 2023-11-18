078ebad4914da0acb83deedddb6cdd00a8a74373
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2023-11-18 00:00:01 -0800

    Update Class Activity 3 - Question 6.cpp

--- ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 6.cpp
+++ ETS0742_15_Kaleb_Mesfin/Class Activity 3 - Question 6.cpp
@@ -1,1 +1,13 @@
+// This program calculates the amount of time it will take to read a file of certain siez
+#include <iostream>
 
+using namespace std;
+
+int main (){
+	float num;
+	cout << "Enter the size of your file in bytes: ";
+	cin >> num;
+	
+	cout << "The amount of time it will take would be: " << num / (sizeof(char) * 960) << " second(s)." << endl;
+	return 0;
+}
